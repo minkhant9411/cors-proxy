2e71d8f29b07e8f2732a0af27c39f8855fd17221
echo <minkhant9411@gmail.com>
2023-10-17 00:07:07 +0630

    nothingmuch

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const request = require('request');
-const app = express();
+const puppeteer = require('puppeteer');
 
-const PORT = 3000; // You can change the port number as needed
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Update the * to your specific domain if necessary
@@ -10,18 +10,23 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/proxy', (req, res) => {
+app.get('/proxy', async (req, res) => {
   const url = req.query.url;
   if (!url) {
     return res.status(400).send('Missing URL parameter.');
   }
 
-  request(url, (error, response, body) => {
-    if (error) {
-      return res.status(500).send(error.message);
-    }
-    res.send(body);
-  });
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const content = await page.content();
+    await browser.close();
+    res.send(content);
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send('Error retrieving data');
+  }
 });
 
 app.listen(PORT, () => {
